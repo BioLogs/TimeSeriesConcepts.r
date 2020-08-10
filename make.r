829a64e417876687db202e459febd57fce50d78a
Gustavo Magaña López <gmaganna.biomed@gmail.com>
2020-08-09 22:04:25 -0500

    :building_construction: Removing magic constants
    I am working on removing all magic constants, using a config file to parse data, specify render options, etc. This will be followed by tidyverse data imports because base read.csv is so slow that it makes me want to cry.

--- make.r
+++ make.r
@@ -1,14 +1,24 @@
 
 # Required packages :
 library(rmarkdown)
+library(RcppTOML)
+library(glue)
+
+# Parse configuration :
+config <- parseToml("config.toml")
+Sys.setenv(RSTUDIO_PANDOC=config$paths$pandoc)
 
 # List of files to render :
-rmd.files <- unlist(lapply(dir("src"), function(x){ file.path("src", x) }))
+rmd.files <- Sys.glob(glue::glue("{config$directories$source}*.Rmd"))
+## Legacy :
+#rmd.files <- unlist(lapply(dir(config$directories$source), function(x){ file.path("src", x) }))
+
 # Render only to html, ommit md :
 render.to.html <- function(x){
-  render(x, output_dir = "build")
+  render(x, output_dir = config$directories$dest, output_format="html_document")
 }
-# Execute, assign to `renderd` to avoid unnecessary verbosity : 
+
+# Execute, assign to `rendered` to avoid unnecessary verbosity : 
 rendered <- lapply(rmd.files, render.to.html)
 
 ## END
